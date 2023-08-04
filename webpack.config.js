1c483149bea3d4f1d4166e1e05605259fca6d380
Shana <shanawus@Shanas-MacBook-Pro.local>
2023-08-04 14:56:41 +0800

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,52 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/app.js", // relative path
-  output: {
-    path: path.join(__dirname, "public"), // absolute path
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/, // to run on every file that ends with .js
-        exclude: /node_modules/, // to avoid the file node_modules
-      },
-      {
-        test: /\.s?css$/, // to run on every file that ends with .scss
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  devtool: "eval-cheap-module-source-map",
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "public"),
+module.exports = (env) => {
+  const isProduction = env === "production";
+
+  return {
+    mode: "development",
+    entry: "./src/app.js", // relative path
+    output: {
+      path: path.join(__dirname, "public"), // absolute path
+      filename: "bundle.js",
     },
-    port: 3000,
-    historyApiFallback: true,
-  },
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/, // to run on every file that ends with .js
+          exclude: /node_modules/, // to avoid the file node_modules
+        },
+        {
+          test: /\.s?css$/, // to run on every file that ends with .scss
+          // use: ["style-loader", "css-loader", "sass-loader"],
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true,
+              },
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [new MiniCssExtractPlugin()],
+    devtool: isProduction ? "source-map" : "inline-source-map",
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "public"),
+      },
+      port: 3000,
+      historyApiFallback: true,
+    },
+  };
 };
